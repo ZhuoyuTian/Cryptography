05856fbcc0f1bb21792dfb8004159ec5dee7cf0f
zoe <zt928018@gmail.com>
2023-01-19 13:59:13 +0000

    substitution

---
+++ substitution.py
@@ -0,0 +1,29 @@
+dict_a = {chr(i): (i-97) for i in range(97, 123)}
+print(dict_a)
+new_dict = {v : k for k, v in dict_a.items()}
+print(new_dict)
+#1. 字母变数字
+def char_to_num(sentance):
+    ob_sentance = list(sentance)
+    ob_num = []
+    for i in range(len(ob_sentance)):
+        ob_num.append(dict_a[ob_sentance[i]])
+    return ob_num
+sentance = "jujtbtfdsf"
+print(char_to_num(sentance))
+ob_num = char_to_num(sentance)
+#2. 暴力破解
+def guess(ob_num):
+    for k in range(25):
+        p_num = []
+        p_char = []
+        for i in ob_num:
+            plain_num = (i-k) % 25
+            p_num.append(plain_num)
+        print(p_num)
+        for i in range(len(p_num)):
+            p_char.append(new_dict[p_num[i]])
+
+        plain_char =  "".join(p_char)
+        print(plain_char)
+guess(ob_num)
